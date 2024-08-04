15ba01124bb6c121700093c4f7572686747121cf
Michael Aguilar <bitflip@slowlink.net>
2024-08-04 15:41:03 -0500

    Update to use new models

--- resume_writer/main.py
+++ resume_writer/main.py
@@ -1,26 +1,20 @@
 import logging
 
 import rich
-from resume_markdown import MarkdownResumeParser
-from resume_model import Personal, Resume
+from models.personal import Personal
+from models.resume import Resume
 
 log = logging.basicConfig(level=logging.DEBUG)
 
-def print_personal(personal : Personal) -> None:
+
+def print_personal(personal: Personal) -> None:
     """Print the personal information."""
-    rich.print(f"[bold]Name:[/bold] {personal.personal_info.name}")
-    rich.print(f"[bold]Email:[/bold] {personal.personal_info.email}")
-    rich.print(f"[bold]Phone:[/bold] {personal.personal_info.phone}")
-
-    _personal_info = personal.personal_info
-    rich.print(f"[bold]Website:[/bold] {_personal_info.website}")
-    rich.print(f"[bold]LinkedIn:[/bold] {_personal_info.linkedin}")
-    rich.print(f"[bold]GitHub:[/bold] {_personal_info.github}")
-    rich.print(f"[bold]Twitter:[/bold] {_personal_info.twitter}")
-    rich.print(f"[bold]Work Authorization:[/bold] {_personal_info.work_authorization}")
+    rich.print(f"[bold]Name:[/bold] {personal.contact_info.name}")
+    rich.print(f"[bold]Email:[/bold] {personal.contact_info.email}")
+    rich.print(f"[bold]Phone:[/bold] {personal.contact_info.phone}")
 
 
-def dump_resume(resume : Resume) -> None:
+def dump_resume(resume: Resume) -> None:
     """Dump the resume to the console."""
 
     _personal = resume.personal
@@ -29,11 +23,16 @@ def dump_resume(resume : Resume) -> None:
 
 def go() -> None:
     """Start the app."""
-    _parser = MarkdownResumeParser("data/test_resume.md")
 
-    _resume = _parser.parse()
+    with open("../tests/test_resume.md") as f:
+        _resume_text = f.read()
+
+    _resume_lines = _resume_text.split("\n")
+    _resume = Resume.parse(_resume_lines)
+
+    # dump the resume to the console
     dump_resume(_resume)
-    rich.print(_resume.stats())
+
 
 if __name__ == "__main__":
     go()
