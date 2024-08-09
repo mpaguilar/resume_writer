62f94322d57ba6eef85812c6b528c99a8822c96d
Michael Aguilar <bitflip@slowlink.net>
2024-08-09 08:55:43 -0500

    Move subsection rendering (skills, basics) to their own classes.

--- resume_writer/docx_render/ats_experience_section.py
+++ resume_writer/docx_render/ats_experience_section.py
@@ -2,12 +2,125 @@ import logging
 from datetime import datetime
 
 import docx.document
-from docx_render.docx_render_base import DocxExperienceBase
+from docx_render.docx_render_base import DocxExperienceBase, DocxRenderBase
 from docx_render.resume_settings import ResumeExperienceSettings
 from models.experience import RoleBasics, RoleSkills
 from models.resume import Resume
 
 log = logging.getLogger(__name__)
+
+
+class ATSRenderExperienceSkillsSection(DocxRenderBase):
+    """Render skills section to a document."""
+
+    def __init__(
+        self,
+        document: docx.document.Document,
+        skills: RoleSkills,
+    ):
+        """Initialize the skills section."""
+
+        assert isinstance(skills, RoleSkills)
+
+        super().__init__()
+        self.document = document
+        self.skills = skills
+
+    def render(self) -> None:
+        """Render skills to the document."""
+        log.debug("Rendering role skills")
+
+        if len(self.skills) == 0:
+            _msg = "No skills to render"
+            self.warnings.append(_msg)
+            log.info(_msg)
+            return
+
+        _skill_text = ", ".join(self.skills)
+
+        self.document.add_paragraph(f"Skills: {_skill_text}")
+
+
+class ATSRenderExperienceRoleBasicsSection(DocxRenderBase):
+    """Render role basics section to a document."""
+
+    def __init__(
+        self,
+        document: docx.document.Document,
+        basics: RoleBasics,
+        settings: ResumeExperienceSettings,
+    ):
+        """Initialize the role basics section."""
+
+        assert isinstance(basics, RoleBasics)
+        # Initialize the base class
+        super().__init__()
+        self.document = document
+        self.basics = basics
+        self.settings = settings
+
+    def render(self) -> None: # noqa: C901
+        """Render role basics to the document."""
+
+        log.debug("Rendering role basics")
+
+        _paragraph_lines = []
+
+        # company name is required
+        if not self.basics.company:
+            _msg = "Company name is required"
+            self.errors.append(_msg)
+            log.warning(_msg)
+
+        _paragraph_lines.append(f"Company: {self.basics.company}")
+
+        # Title is rendered by calling function
+        if not self.basics.title:
+            _msg = "Title is required"
+            self.errors.append(_msg)
+            log.warning(_msg)
+
+        _paragraph_lines.append(f"Title: {self.basics.title}")
+
+        # job category
+        if self.basics.job_category and self.settings.job_category:
+            _paragraph_lines.append(f"Job Category: {self.basics.job_category}")
+
+        if self.basics.location and self.settings.location:
+            _paragraph_lines.append(f"Location: {self.basics.location}")
+
+        if self.basics.agency_name and self.settings.accomplishments:
+            _paragraph_lines.append(f"Agency: {self.basics.agency_name}")
+
+        if self.basics.employment_type and self.settings.employment_type:
+            _paragraph_lines.append(f"Employment Type: {self.basics.employment_type}")
+
+        # Start date
+        if not self.basics.start_date:
+            _msg = "Start date is required"
+            self.errors.append(_msg)
+            log.warning(_msg)
+
+        _value = datetime.strftime(self.basics.start_date, "%B %Y")
+        _paragraph_lines.append(f"Start Date: {_value}")
+
+        # End date
+        if self.basics.end_date:
+            _value = datetime.strftime(self.basics.end_date, "%B %Y")
+            _paragraph_lines.append(f"End Date: {_value}")
+
+        # Location
+        if self.basics.location:
+            _paragraph_lines.append(f"Location: {self.basics.location}")
+
+        # Reason for change
+        if self.basics.reason_for_change and self.settings.reason_for_leaving:
+            _paragraph_lines.append(
+                f"Reason for change: {self.basics.reason_for_change}",
+            )
+
+        if len(_paragraph_lines) > 0:
+            self.document.add_paragraph("\n".join(_paragraph_lines))
 
 
 class ATSExperienceSection(DocxExperienceBase):
@@ -23,85 +136,6 @@ class ATSExperienceSection(DocxExperienceBase):
 
         super().__init__(document=document, resume=resume, settings=settings)
 
-    def basics(self, basics: RoleBasics) -> None:  # noqa: C901
-        """Render role basics to the document."""
-
-        assert isinstance(basics, RoleBasics)
-
-        log.debug("Rendering role basics")
-
-        _paragraph_lines = []
-
-        # company name is required
-        if not basics.company:
-            _msg = "Company name is required"
-            self.errors.append(_msg)
-            log.warning(_msg)
-
-        _paragraph_lines.append(f"Company: {basics.company}")
-
-        # Title is rendered by calling function
-        if not basics.title:
-            _msg = "Title is required"
-            self.errors.append(_msg)
-            log.warning(_msg)
-
-        _paragraph_lines.append(f"Title: {basics.title}")
-
-        if basics.job_category and self.settings.job_category:
-            _paragraph_lines.append(f"Job Category: {basics.job_category}")
-
-        if basics.location and self.settings.location:
-            _paragraph_lines.append(f"Location: {basics.location}")
-
-        if basics.agency_name and self.settings.accomplishments:
-            _paragraph_lines.append(f"Agency: {basics.agency_name}")
-
-        if basics.employment_type and self.settings.employment_type:
-            _paragraph_lines.append(f"Employment Type: {basics.employment_type}")
-
-        # Start date
-        if not basics.start_date:
-            _msg = "Start date is required"
-            self.errors.append(_msg)
-            log.warning(_msg)
-
-        _value = datetime.strftime(basics.start_date, "%B %Y")
-        _paragraph_lines.append(f"Start Date: {_value}")
-
-        # End date
-        if basics.end_date:
-            _value = datetime.strftime(basics.end_date, "%B %Y")
-            _paragraph_lines.append(f"End Date: {_value}")
-
-        if basics.location:
-            _paragraph_lines.append(f"Location: {basics.location}")
-
-        # Reason for change
-        if basics.reason_for_change and self.settings.reason_for_leaving:
-            _paragraph_lines.append(f"Reason for change: {basics.reason_for_change}")
-
-        if len(_paragraph_lines) > 0:
-            self.document.add_paragraph("\n".join(_paragraph_lines))
-
-
-    def skills(self, skills: RoleSkills) -> None:
-        """Render role skills to the document."""
-
-        assert isinstance(skills, RoleSkills)
-
-        log.debug("Rendering role skills")
-
-        if len(skills) == 0:
-            _msg = "No skills to render"
-            self.warnings.append(_msg)
-            log.info(_msg)
-            return
-
-        _skill_text = ", ".join(skills)
-
-        self.document.add_paragraph(f"Skills: {_skill_text}")
-
     def roles(self) -> None:
         """Render roles to the document."""
 
@@ -113,7 +147,12 @@ class ATSExperienceSection(DocxExperienceBase):
                 self.errors.append("Role title is required")
 
             self.document.add_heading(_role.basics.title, 2)
-            self.basics(_role.basics)
+            _basics = ATSRenderExperienceRoleBasicsSection(
+                document=self.document,
+                basics=_role.basics,
+                settings=self.settings,
+            )
+            _basics.render()
 
             if _role.summary and self.settings.summary:
                 self.document.add_paragraph(_role.summary.summary)
@@ -122,7 +161,8 @@ class ATSExperienceSection(DocxExperienceBase):
                 self.document.add_paragraph(_role.responsibilities.text)
 
             if _role.skills and self.settings.skills:
-                self.skills(_role.skills)
+                _skills = ATSRenderExperienceSkillsSection(self.document, _role.skills)
+                _skills.render()
 
     def render(self) -> None:
         """Render the section to the document."""
