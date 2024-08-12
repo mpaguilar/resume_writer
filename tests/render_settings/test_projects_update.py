6099bfef0ee872626ca10ddc10fcc6939a6a2a94
Michael Aguilar <bitflip@slowlink.net>
2024-08-12 10:18:18 -0500

    Update and add coverage

--- tests/render_settings/test_projects_update.py
+++ tests/render_settings/test_projects_update.py
@@ -1,14 +1,14 @@
 from resume_writer.resume_render.render_settings import (
-    ResumeRolesSettings,
+    ResumeProjectsSettings,
 )
 
 
-class TestResumeRolesSettings:
+class TestResumeProjectsSettings:
     def setup_method(self):
-        self.settings = ResumeRolesSettings()
+        self.settings = ResumeProjectsSettings()
 
     def test_update_from_dict(self):
-        data_dict = {"summary": False, "skills": False}
+        data_dict = {"overview": False, "skills": False}
         self.settings.update_from_dict(data_dict)
-        assert self.settings.summary is False
+        assert self.settings.overview is False
         assert self.settings.skills is False
