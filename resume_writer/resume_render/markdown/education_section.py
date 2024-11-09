db88fd95a5cd5938e95c3def1cd1209278151c12
Michael Aguilar <bitflip@slowlink.net>
2024-11-09 10:43:56 -0600

    Copied html to markdown

---
+++ resume_writer/resume_render/markdown/education_section.py
@@ -0,0 +1,46 @@
+import logging
+
+from jinja2 import Environment
+
+from resume_writer.models.education import Education
+from resume_writer.resume_render.render_settings import ResumeEducationSettings
+from resume_writer.resume_render.resume_render_text_base import (
+    ResumeRenderEducationBase,
+)
+from resume_writer.utils.html_doc import HtmlDoc
+
+log = logging.getLogger(__name__)
+
+class RenderEducationSection(ResumeRenderEducationBase):
+    """Render Education Section."""
+
+    def __init__(
+        self,
+        document: HtmlDoc,
+        jinja_env: Environment,
+        education: Education,
+        settings: ResumeEducationSettings,
+    ):
+        """Initialize the basic education renderer."""
+        super().__init__(
+            document=document,
+            jinja_env=jinja_env,
+            education=education,
+            template_name="education.j2",
+            settings=settings,
+        )
+
+    def render(self) -> None:
+        """Render the education section."""
+
+        if not self.settings.degrees:
+            log.debug("No degrees to render.")
+            return
+
+        log.debug("Rendering education section.")
+
+        _rendered = self.template.render(
+            settings=self.settings, education=self.education,
+        )
+
+        self.document.add_text(_rendered)
---
+++ resume_writer/resume_render/markdown/resume_main.py
@@ -0,0 +1,99 @@
+import logging
+
+from jinja2 import Environment
+
+from resume_writer.models.resume import Resume
+from resume_writer.resume_render.html.certifications_section import (
+    RenderCertificationsSection,
+)
+from resume_writer.resume_render.html.education_section import (
+    RenderEducationSection,
+)
+from resume_writer.resume_render.html.experience_section import (
+    RenderExperienceSection,
+)
+from resume_writer.resume_render.html.personal_section import (
+    RenderPersonalSection,
+)
+from resume_writer.resume_render.render_settings import ResumeRenderSettings
+from resume_writer.resume_render.resume_render_text_base import (
+    ResumeRenderBase,
+)
+from resume_writer.utils.html_doc import HtmlDoc
+
+log = logging.getLogger(__name__)
+
+
+class RenderResume(ResumeRenderBase):
+    """Render a resume in basic format."""
+
+    def __init__(
+        self,
+        document: HtmlDoc,
+        jinja_env: Environment,
+        resume: Resume,
+        settings: ResumeRenderSettings,
+    ):
+        """Initialize basic resume renderer."""
+        super().__init__(
+            document=document,
+            jinja_env=jinja_env,
+            resume=resume,
+            settings=settings,
+        )
+
+    def render(self) -> None:
+        """Render the resume."""
+
+        if self.resume.personal and self.settings.personal:
+            RenderPersonalSection(
+                document=self.document,
+                personal=self.resume.personal,
+                jinja_env=self.jinja_env,
+                settings=self.settings.personal_settings,
+            ).render()
+
+        if self.resume.education and self.settings.education:
+            RenderEducationSection(
+                document=self.document,
+                jinja_env=self.jinja_env,
+                education=self.resume.education,
+                settings=self.settings.education_settings,
+            ).render()
+
+        if self.resume.certifications and self.settings.certifications:
+            RenderCertificationsSection(
+                document=self.document,
+                jinja_env=self.jinja_env,
+                certifications=self.resume.certifications,
+                settings=self.settings.certifications_settings,
+            ).render()
+
+        if self.resume.experience and self.settings.experience:
+            RenderExperienceSection(
+                document=self.document,
+                jinja_env=self.jinja_env,
+                experience=self.resume.experience,
+                settings=self.settings.experience_settings,
+            ).render()
+
+
+"""
+        # the executive summary is built from experience, so it has to exist
+        if self.resume.experience and self.settings.executive_summary:
+            self.document.add_heading("Executive Summary", 2)
+
+            RenderExecutiveSummarySection(
+                self.document,
+                self.resume.experience,
+                self.settings.executive_summary_settings,
+            ).render()
+
+        # the skills section is built from experience, so it has to exist
+        if self.resume.experience and self.settings.skills_matrix:
+            RenderSkillsMatrixSection(
+                self.document,
+                self.resume.experience,
+                self.settings.skills_matrix_settings,
+            ).render()
+"""
